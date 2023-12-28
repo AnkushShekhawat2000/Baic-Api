e966fabf24bc37a79b52005984ba7e2576337008
Ankush Shekhawat <ankush75056@gmail.com>
2023-12-28 21:30:46 +0530

    chat App

---
+++ server.js
@@ -0,0 +1,9 @@
+// we are importing express from node_modules
+
+const express = require('express');
+// this express is a fumction which when invoked give me server
+
+const server = express();
+
+server.listen(8888);
+
